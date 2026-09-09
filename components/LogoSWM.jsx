be4e1aaf669dc56ed1187360b355c597c1d3d236
fedegarelli2000-sudo <fedegarelli2000@gmail.com>
2026-09-09 03:14:52 -0300

    Update LogoSWM.jsx

--- components/LogoSWM.jsx
+++ components/LogoSWM.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function LogoSWM({ size = 'medium', className = '' }) {
+export default function LogoMM({ size = 'medium', className = '' }) {
   const dimensions = {
     small: { width: 120, height: 40, fontSize: '22px' },
     medium: { width: 180, height: 60, fontSize: '32px' },
@@ -17,7 +17,7 @@ export default function LogoSWM({ size = 'medium', className = '' }) {
         xmlns="http://www.w3.org/2000/svg"
         className="drop-shadow-md"
       >
-        {/* M Gris (Fondo - Ligeramente detrás) */}
+        {/* M Gris (Fondo - Detrás) */}
         <path
           d="M 10 70 L 10 20 L 30 50 L 50 20 L 50 70"
           stroke="#9CA3AF"
@@ -40,7 +40,7 @@ export default function LogoSWM({ size = 'medium', className = '' }) {
           className="font-extrabold tracking-widest leading-none text-zinc-100"
           style={{ fontSize: dimensions.fontSize }}
         >
-          SWM
+          MM
         </span>
         <span className="text-[10px] tracking-widest text-orange-500 uppercase font-semibold mt-1">
           Estudio Jurídico
